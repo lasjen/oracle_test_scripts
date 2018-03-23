af3eef869f3b41e4de39f62e1f73a5e42ec4fa2a
Lasse Jenssen <lasse@jcon.no>
2018-03-23 13:23:32 +0000

    JdbcBatchInsert

---
+++ java_tests/JdbcBatchInsert.java
@@ -0,0 +1,36 @@
+import java.sql.*;
+import oracle.jdbc.OracleConnection;
+import oracle.sql.ArrayDescriptor;
+import oracle.sql.ARRAY;
+
+public class JdbcBatchInsert {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int obj_id = 0;
+	  int count = 0;
+
+    Connection conn = null;
+    DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+    conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:151/orcl","devdata","dev");
+    conn.setAutoCommit(false);
+
+    // Debug
+    PreparedStatement ps = conn.prepareStatement(
+         "INSERT INTO tst VALUES (?,?)");
+
+    for (int i=1;i<10;i++){
+       ps.setInt(1,i);
+       ps.setString(2,"XXXX");
+       ps.addBatch();
+    }
+    ps.executeBatch();
+		conn.commit();
+    ps.close();
+
+	}
+
+}
